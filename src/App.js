6281f452ee6a7baa17894e0edb14c21c57f2408c
HeoJiye <heoze1031@gmail.com>
2023-04-09 03:08:53 +0900

    feat: authenticate mail

--- src/App.js
+++ src/App.js
@@ -5,19 +5,54 @@ import "./App.css";
 function App() {
   const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    fetch("/api/example")
+  let userId;
+  let authCode;
+  const sendEmail = (email) => {
+    fetch(`/email/password/${email}`)
       .then((response) => response.text())
       .then((message) => {
-        setMessage(message);
+        userId = message[0];
+        authCode = message[1];
+
+        alert("메일을 보냈습니다! 인증 번호를 입력해주세요!");
       });
-  }, []);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
+        <p>
+          <form
+            id="emailForm"
+            onSubmit={(e) => {
+              e.preventDefault();
+
+              const email = e.target.childNodes[1].value;
+              sendEmail(email);
+            }}
+          >
+            <div>이메일을 입력하세요</div>
+            <input type="text"></input>
+            <input type="submit" value="제출"></input>
+          </form>
+        </p>
+        <p>
+          <form
+            id="AuthForm"
+            onSubmit={(e) => {
+              e.preventDefault();
+
+              const code = e.target.childNodes[1].value;
+              if (authCode === code) alert("인증에 성공했습니다!");
+              else alert("인증에 실패했습니다!");
+            }}
+          >
+            <div>인증번호를 입력하세요</div>
+            <input type="text"></input>
+            <input type="submit" value="제출"></input>
+          </form>
+        </p>
       </header>
     </div>
   );
