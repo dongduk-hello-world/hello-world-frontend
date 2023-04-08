028e9fa24a3d04c1af07fa790c097cb934f6e454
HeoJiye <heoze1031@gmail.com>
2023-04-08 23:44:06 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api/example")
+      .then((response) => response.text())
+      .then((message) => {
+        setMessage(message);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{message}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
