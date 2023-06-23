b79139e2f962f1582c937f9e720194456a8affeb
HeoJiye <heoze1031@gmail.com>
2023-06-23 21:39:43 +0900

    home lecture list & join

--- src/pages/Lecture/hooks.js
+++ src/pages/Lecture/hooks.js
@@ -3,7 +3,15 @@ import axiosPromise from "../../services/axiosPromise";
 
 export const getUser = async (userId) => {
   const result = await axiosPromise.get(`/users/${userId}`);
-  // console.log(result);
   return result;
 }
 
+export const getLectureLsit = async () => {
+  const result = await axiosPromise.get(`/classes`);
+  return result;
+}
+
+export const joinLecture = async (classId) => {
+  const userId = sessionStorage.getItem('userId');
+  const result = await axiosPromise.post(`/classes/${classId}/students/${userId}`);
+}
