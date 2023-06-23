c480dc404bd2756068823ffc010c3efdb91b3273
HeoJiye <heoze1031@gmail.com>
2023-06-23 13:47:06 +0900

    assignment result view 수정 + 코드 채점 완료 시 알림

--- src/pages/assignment/hooks.js
+++ src/pages/assignment/hooks.js
@@ -25,6 +25,7 @@ export const loadAssignment = async ( assignmentId ) => {
 
 export const submit = async (testId, language, code) => {
     let data = await axiosPromise.post(`/tests/${testId}/submits`, {language, code});
+    alert("채점이 완료되었습니다. \n기록 창에서 확인하세요.")
     return data;
 };
 
@@ -33,42 +34,14 @@ const createData = (id, submitTime, language, errMsg, runTime, fileSize, score) 
 };
 
 export const getResult = async (testId) => {
-    let defaultData = {};
-    defaultData.highScore = { score: "500/500", code: {
-        language: "c", 
-        code:
-`#include <stdio.h>
-
-int main(void) {
-printf("Hello World");
-}`
-    }};
-    defaultData.submits = [];
-    const length = Math.floor(Math.random() * 20);
-    for (let i = 0; i < length; i++) {
-        defaultData.submits.push(createData(i, "2023-03-19 AM 11:00", "Java", null, "이정도", "이정도", 50));
-    }
-
-    let data = await axiosPromise.get(`/tests/${testId}/submits`, defaultData);
+    let data = await axiosPromise.get(`/tests/${testId}/submits`);
 
     console.log(data);
-    console.log(data.highScore.score);
     return data;
 };
 
 export const getCode = async (testId, index) => {
-    let data = await axiosPromise.get(`/tests/${testId}/submits/${index}`, 
-        {
-            language: "c",
-            code: 
-`#include <stdio.h>
-    
-int main(void) {
-    print("Hello World");
-}`,
-            error: `print()가 아니라 printf()지롱~` 
-        });
-
+    let data = await axiosPromise.get(`/tests/${testId}/submits/${index}`);
     return data;
 }
 
