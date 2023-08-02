43354b8d698c77752789404668f4895cd2bd04cd
Azamman327 <wjsdbdud327@naver.com>
2023-08-02 20:08:58 +0900

    reset

--- src/pages/Lecture/professor/Form/hooks.js
+++
@@ -1,8 +0,0 @@
-import { useState, useEffect, useRef } from "react";
-import axiosPromise from "../../../../services/axiosPromise";
-
-export const submitForm = async (data) => {
-  const result = await axiosPromise.post(`/classes`, data);
-  // console.log(result);
-  // return result;
-}
--- src/pages/Lecture/student/styles.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from "@material-ui/core/styles";
-import { SosOutlined } from "@mui/icons-material";
-
-export const useStyles = makeStyles({
-    cardActions: {
-        justifyContent: "right"
-    }
-});
