c7f37805d5690edd4c4011e5a0673ddffb24ad8b
Azamman327 <wjsdbdud327@naver.com>
2024-01-03 22:34:32 +0900

    add Typography

--- src/pages/result/infoTable.jsx
+++ src/pages/result/infoTable.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 export default function BasicTable() {
   return (
@@ -11,13 +12,14 @@ export default function BasicTable() {
           mb: 10
       }}
     >
-      <Paper 
-        elevation={3}
+      <Paper elevation={3}
         sx={{
           p: 5,
           minHeight: 150
-        }}>
-          test
+        }}
+      >
+          <Typography variant='h3'>[20202020] 님의 [과제1] 득점은 총 [170]점 입니다.</Typography>
+          <Typography variant='h5'>총점 250점 중 170점 획득</Typography>
         </Paper>
     </Box>
   );
