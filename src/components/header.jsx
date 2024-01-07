5f164cbddd67a8569b28f632b0858584733e64cd
Azamman327 <wjsdbdud327@naver.com>
2024-01-07 17:02:04 +0900

    remake header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,46 +5,62 @@ import Stack from '@mui/material/Stack';
 
 const Header = () => {
   return (
-    <Grid 
-      container 
-      className={styles.header} 
-      justifyContent="space-between"
-      alignItems="center"
-    >
-      <Grid 
-        xs={2}  
-        className={styles.logo} 
-        justifyContent="left"
-      >
-        Helloworld
-      </Grid>
-      <Grid 
-        container
-        xs={7} 
-        justifyContent="space-evenly"
-      >
-        <span>내 과목</span>
-        <span>과목 찾기</span>
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <img src={`${process.env.PUBLIC_URL}/public_assets/logo/logo_black.png`} alt="logo_black"/>
+      </div>
+      <nav className={styles.menu}>
+        <li>내 과목</li>
+        <li>과목 찾기</li>
         {/* 삼항연산자(user_type == professor) */}
-        <span>과목 개설</span>
-      </Grid>
-      <Grid 
-        container
-        xs={3} 
-        justifyContent="right"
-        alignItems="center"
-      >
-        <span className={styles.profile}>id / 이름</span>
-        <Stack
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <img src={`${process.env.PUBLIC_URL}/public_assets/logout.png`} alt="logout"/>
-          로그아웃
-        </Stack>
-      </Grid>
-    </Grid>
+        <li>과목 개설</li>
+      </nav>
+      <div>
+
+      </div>
+    </div>
+    // <Grid 
+    //   container 
+    //   className={styles.header} 
+    //   justifyContent="space-between"
+    //   alignItems="center"
+    // >
+    //   <Grid 
+    //     xs={2}  
+    //     className={styles.logo} 
+    //     justifyContent="left"
+    //   >
+    //     <div className={styles.logo}>
+    //       <img src={`${process.env.PUBLIC_URL}/public_assets/logo/logo_black.png`} alt="logo_black"/>
+    //     </div>
+    //   </Grid>
+    //   <Grid 
+    //     container
+    //     xs={7} 
+    //     justifyContent="space-evenly"
+    //   >
+    //     <span>내 과목</span>
+    //     <span>과목 찾기</span>
+    //     {/* 삼항연산자(user_type == professor) */}
+    //     <span>과목 개설</span>
+    //   </Grid>
+    //   <Grid 
+    //     container
+    //     xs={3} 
+    //     justifyContent="right"
+    //     alignItems="center"
+    //   >
+    //     <span className={styles.profile}>id / 이름</span>
+    //     <Stack
+    //       direction="row"
+    //       justifyContent="center"
+    //       alignItems="center"
+    //     >
+    //       <img src={`${process.env.PUBLIC_URL}/public_assets/logout.png`} alt="logout" className={styles.logout}/>
+    //       로그아웃
+    //     </Stack>
+    //   </Grid>
+    // </Grid>
   );
 }
 
